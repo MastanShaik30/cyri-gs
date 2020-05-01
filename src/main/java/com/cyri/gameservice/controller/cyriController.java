99ec5624fe42d15c32ff32bb0eefed03753cddd9
Mastan Shaik <mastan30@gmail.com>
2020-05-01 18:44:17 -0400

    modified pom and controller to test docker

--- src/main/java/com/cyri/gameservice/controller/cyriController.java
+++ src/main/java/com/cyri/gameservice/controller/cyriController.java
@@ -2,17 +2,18 @@ package com.cyri.gameservice.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class cyriController {
 	 private static final Logger logger = LogManager.getLogger(cyriController.class);
-
+	 	
 	    @GetMapping("/welcome")
 	    public String welcome() {
             logger.debug("Inside welcome method of controller");
-	        return "welcome"; 
+	        return "Game Service welcome"; 
 	    }
 }
