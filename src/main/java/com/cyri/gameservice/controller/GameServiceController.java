3508d12887cc9f23fd9f028bd5107d81d60aa998
Mastan Shaik <mastan30@gmail.com>
2020-05-01 21:25:44 -0400

    modified the name of controller

--- src/main/java/com/cyri/gameservice/controller/GameServiceController.java
+++ src/main/java/com/cyri/gameservice/controller/GameServiceController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-public class cyriController {
-	 private static final Logger logger = LogManager.getLogger(cyriController.class);
+public class GameServiceController {
+	 private static final Logger logger = LogManager.getLogger(GameServiceController.class);
 	 	
 	    @GetMapping("/welcome")
 	    public String welcome() {
